c27bd602b828af484458469a79b8e0dae54a54f3
ChavesYuri <33982282+ChavesYuri@users.noreply.github.com>
2025-03-20 14:09:22 -0300

    route when theres no player and when there's player and it answers

--- QuizEngine/Flow.swift
+++ QuizEngine/Flow.swift
@@ -2,24 +2,45 @@ import Foundation
 
 public protocol Router {
     typealias Question = String
-    func routeTo(question: Question)
-    func routeToResult()
+    typealias Answer = (String) -> Void
+    func routeToPlayerTurn(player: Player, _ onStart: @escaping () -> Void)
+    func routeToQuestionScreen(_ question: Question, _ answer: @escaping Answer)
+    func routeToQuestionResult()
+    func routeToGameResult()
+}
+
+public struct Player: Equatable {
+    let name: String
+    var score: Int = 0
+    
+    public init(name: String) {
+        self.name = name
+    }
 }
 
 public final class Flow {
+    private let players: [Player]
     private let router: Router
     private let questions: [String]
     
-    public init(router: Router, questions: [String]) {
+    
+    public init(players: [Player], router: Router, questions: [String]) {
+        self.players = players
         self.router = router
         self.questions = questions
     }
     
     public func start() {
-        if let firstQuestion = questions.first {
-            router.routeTo(question: firstQuestion)
+        if let firstQuestion = questions.first, let firstPlayer = players.first {
+            router.routeToPlayerTurn(player: firstPlayer, { [weak self] in
+                self?.routeToQuestion(firstQuestion, player: firstPlayer)
+            })
         } else {
-            router.routeToResult()
+            router.routeToGameResult()
         }
+    }
+    
+    private func routeToQuestion(_ question: Router.Question, player: Player) {
+        router.routeToQuestionScreen(question, { _ in })
     }
 }
--- QuizEngineTests/FlowTest.swift
+++ QuizEngineTests/FlowTest.swift
@@ -3,34 +3,87 @@ import XCTest
 import QuizEngine
 
 final class FlowTest: XCTestCase {
-    func test_start_noQuestions_routesToResult() {
-        let (sut, routerSpy) = makeSUT(questions: [])
+    func test_start_noQuestionsAndOnePlayer_routesToGameResult() {
+        let (sut, routerSpy) = makeSUT(questions: [], players: [.init(name: "a player")])
         
         sut.start()
         
-        XCTAssertTrue(routerSpy.routedQuestions.isEmpty)
-        XCTAssertEqual(routerSpy.routedToResultCallCount, 1)
+        XCTAssertTrue(routerSpy.routedQuestionsRequests.isEmpty)
+        XCTAssertTrue(routerSpy.routedQuestionsRequests.isEmpty)
+        XCTAssertEqual(routerSpy.routedToGameResultCallCount, 1)
+    }
+    
+    func test_start_withOneQuestionAndNoPlayers_routesToGameResult() {
+        let (sut, routerSpy) = makeSUT(questions: ["Q1"], players: [])
+        
+        sut.start()
+        
+        XCTAssertTrue(routerSpy.routedQuestionsRequests.isEmpty)
+        XCTAssertTrue(routerSpy.routedQuestionsRequests.isEmpty)
+        XCTAssertEqual(routerSpy.routedToGameResultCallCount, 1)
+    }
+    
+    func test_start_withNoQuestionAndNoPlayers_routesToGameResult() {
+        let (sut, routerSpy) = makeSUT(questions: [], players: [])
+        
+        sut.start()
+        
+        XCTAssertTrue(routerSpy.routedQuestionsRequests.isEmpty)
+        XCTAssertTrue(routerSpy.routedQuestionsRequests.isEmpty)
+        XCTAssertEqual(routerSpy.routedToGameResultCallCount, 1)
+    }
+    
+    func test_start_withOneQuestionAndOnePlayer_routesToFirstPlayerTurn() {
+        let players: [Player] = [.init(name: "a player")]
+        let (sut, routerSpy) = makeSUT(questions: ["Q1"], players: players)
+        
+        sut.start()
+        
+        XCTAssertEqual(routerSpy.routedToPlayerTurnRequests[0].player, players[0])
+        XCTAssertEqual(routerSpy.routedToGameResultCallCount, 0)
+        XCTAssertTrue(routerSpy.routedQuestionsRequests.isEmpty)
+    }
+    
+    func test_startAndFirstPlayerInitiatesGame_withOneQuestionAndOnePlayer_routesToFirstQuestion() {
+        let players: [Player] = [.init(name: "a player")]
+        let (sut, routerSpy) = makeSUT(questions: ["Q1"], players: players)
+        
+        sut.start()
+        routerSpy.routedToPlayerTurnRequests[0].onStart()
+        
+        XCTAssertEqual(routerSpy.routedToPlayerTurnRequests[0].player, players[0])
+        XCTAssertEqual(routerSpy.routedToGameResultCallCount, 0)
+        XCTAssertEqual(routerSpy.routedQuestionsRequests.count, 1)
+        XCTAssertEqual(routerSpy.routedQuestionsRequests.first?.question, "Q1")
     }
     
     private func makeSUT(
-        questions: [String] = []
+        questions: [String] = [],
+        players: [Player] = []
     ) -> (Flow, RouterSpy) {
         let routerSpy = RouterSpy()
-        let sut = Flow(router: routerSpy, questions: questions)
+        let sut = Flow(players: players, router: routerSpy, questions: questions)
         
         return (sut, routerSpy)
     }
     
     final class RouterSpy: Router {
-        var routedToResultCallCount = 0
-        var routedQuestions: [Question] = []
+        var routedToGameResultCallCount = 0
+        var routedQuestionsRequests: [(question: Question, completion: Answer)] = []
+        var routedToPlayerTurnRequests: [(player: Player, onStart: () -> Void)] = []
         
-        func routeTo(question: Question) {
-            routedQuestions.append(question)
+        func routeToPlayerTurn(player: Player, _ onStart: @escaping () -> Void) {
+            routedToPlayerTurnRequests.append((player, onStart))
         }
         
-        func routeToResult() {
-            routedToResultCallCount += 1
+        func routeToQuestionScreen(_ question: Question, _ answer: @escaping Answer) {
+            routedQuestionsRequests.append((question, answer))
+        }
+        
+        func routeToQuestionResult() {}
+        
+        func routeToGameResult() {
+            routedToGameResultCallCount += 1
         }
     }
     
